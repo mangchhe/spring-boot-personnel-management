2e0a9b6fc5d72d4775e8722a1107ffe205d6a3cd
HaJooHyunDesktop <mangchhe@naver.com>
2020-12-29 14:41:56 +0900

    Spring Boot 초기 설정

---
+++ src/main/java/team/okky/personnel_management/PersonnelManagementApplication.java
@@ -0,0 +1,13 @@
+package team.okky.personnel_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonnelManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonnelManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/team/okky/personnel_management/PersonnelManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package team.okky.personnel_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonnelManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

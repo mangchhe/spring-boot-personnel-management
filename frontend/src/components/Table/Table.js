65412adf1f702fe771ca048240dc383b4b71a66c
danakim21 <danakim21@gmail.com>
2020-12-31 21:11:28 +0900

    Add header, table, font

--- frontend/src/components/Table/Table.js
+++ frontend/src/components/Table/Table.js
@@ -1,7 +1,35 @@
 import React from 'react';
 
-function Table() {
-  return <div>Table</div>;
+const AttendanceRow = ({ data }) => {
+  return (
+    <tr>
+      <td>{data.date}</td>
+      <td>{data.day}</td>
+      <td>{data.name}</td>
+      <td>{data.team}</td>
+      <td>{data.rank}</td>
+      <td>{data.startTime}</td>
+      <td>{data.endTime}</td>
+      <td>{data.status}</td>
+    </tr>
+  );
+};
+
+function Table({ page, headerArr, dataArr }) {
+  return (
+    <table>
+      <tr>
+        {headerArr.map((header) => (
+          <th>{header}</th>
+        ))}
+      </tr>
+      {dataArr.map((data) => {
+        if (page === 'attendance') {
+          return <AttendanceRow data={data} />;
+        }
+      })}
+    </table>
+  );
 }
 
 export default Table;
--- frontend/src/pages/Attendance/Attendance.js
+++ frontend/src/pages/Attendance/Attendance.js
@@ -1,7 +1,100 @@
 import React from 'react';
+import Table from '../../components/Table/Table';
+import {
+  FaCheckCircle,
+  FaExclamationTriangle,
+  FaMinusCircle,
+} from 'react-icons/fa';
+import styles from './attendance.module.css';
+
+const NumBox = ({ num, type }) => {
+  return (
+    <div className={styles.numBox}>
+      <div
+        className={
+          type === '정상 출근'
+            ? styles.normal
+            : type === '지각'
+            ? styles.late
+            : styles.off
+        }
+      >
+        {type === '정상 출근' ? (
+          <FaCheckCircle />
+        ) : type === '지각' ? (
+          <FaExclamationTriangle />
+        ) : (
+          <FaMinusCircle />
+        )}
+      </div>
+
+      <div className={styles.text}>
+        <p>{type}</p>
+        <p>{num}건</p>
+      </div>
+    </div>
+  );
+};
 
 function Attendance() {
-  return <div>attendance</div>;
+  const headerArr = [
+    '일자',
+    '요일',
+    '이름',
+    '부서',
+    '직급',
+    '출근시각',
+    '퇴근시각',
+    '상태',
+  ];
+
+  ////////////////////////////////////////////////
+  ////////////////   Dummy Data      /////////////
+  ////////////////////////////////////////////////
+  const dataArr = [
+    {
+      date: '02/28',
+      day: '금',
+      name: '조혜련',
+      team: '팀원',
+      rank: '사원',
+      startTime: '9:00',
+      endTime: '18:00',
+      status: '정상',
+    },
+    {
+      date: '03/29',
+      day: '토',
+      name: '조혜련',
+      team: '팀원',
+      rank: '사원',
+      startTime: '9:00',
+      endTime: '18:00',
+      status: '지각',
+    },
+    {
+      date: '04/30',
+      day: '일',
+      name: '조혜련',
+      team: '팀원',
+      rank: '사원',
+      startTime: '9:00',
+      endTime: '18:00',
+      status: '휴가',
+    },
+  ];
+  const numArr = { normal: 11, late: 3, off: 2 };
+
+  return (
+    <div>
+      <div className={styles.numBoxContainer}>
+        <NumBox num={numArr.normal} type="정상 출근" />
+        <NumBox num={numArr.late} type="지각" />
+        <NumBox num={numArr.off} type="휴가" />
+      </div>
+      <Table page="attendance" headerArr={headerArr} dataArr={dataArr} />
+    </div>
+  );
 }
 
 export default Attendance;

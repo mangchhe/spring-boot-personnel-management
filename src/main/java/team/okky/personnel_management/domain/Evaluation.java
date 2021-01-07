e24be94bc43b9d6cecce7e506c6c33d4921f3f6f
songyen <yeeun4150@gmail.com>
2021-01-07 17:59:34 +0900

    fix 도메인 수정, 업무관리 도메인, 리포지토리, dto, 서비스, 테스트 구현 완료

--- src/main/java/team/okky/personnel_management/domain/Evaluation.java
+++ src/main/java/team/okky/personnel_management/domain/Evaluation.java
@@ -1,6 +1,7 @@
 package team.okky.personnel_management.domain;
 
 import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 
@@ -13,8 +14,10 @@ public class Evaluation {
     int eval_result_score;
     String eval_comment;
 
-    @ManyToOne(fetch = FetchType.LAZY) @JoinColumn(name = "dept_id")
+    @ManyToOne(fetch = FetchType.LAZY) @JoinColumn(name = "work_id")
     Work work;
+
     @ManyToOne(fetch = FetchType.LAZY) @JoinColumn(name = "emp_id")
     Employee employee;
+
 }

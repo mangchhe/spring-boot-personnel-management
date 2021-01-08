d8b6f5935e32dbbd05d8a0d6b44d77da8ea125a0
HaJooHyun <mangchhe@naver.com>
2021-01-08 15:12:43 +0900

    Update StartConfig [더미 데이터 추가], Attendance 서비스 [출근, 퇴근 시각 로직 추가]

--- src/main/java/team/okky/personnel_management/batch/StartConfig.java
+++ src/main/java/team/okky/personnel_management/batch/StartConfig.java
@@ -1,14 +1,85 @@
 package team.okky.personnel_management.batch;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import team.okky.personnel_management.domain.Department;
+import team.okky.personnel_management.domain.Employee;
+import team.okky.personnel_management.domain.Sick;
+import team.okky.personnel_management.domain.Vacation;
+import team.okky.personnel_management.repository.EmployeeRepository;
+import team.okky.personnel_management.repository.SickRepository;
+import team.okky.personnel_management.repository.VacationRepository;
+import team.okky.personnel_management.service.AttendanceService;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 @Transactional
+@RequiredArgsConstructor
 public class StartConfig implements CommandLineRunner {
+
+    private final EmployeeRepository employeeRepository;
+    private final AttendanceService attendanceService;
+    private final VacationRepository vacationRepository;
+    private final SickRepository sickRepository;
+
     @Override
     public void run(String... args) throws Exception {
+        String[] position = new String[]{"대리", "사원", "부장", "본부장", "사장", "차장", "과장"};
+        List<Employee> employeeList = new ArrayList<>();
+        for (int i = 0; i < 49; i++) {
+            Employee employee = Employee.builder()
+                    .emp_position(position[i % 7])
+                    .emp_name("테스터" + i)
+                    .department(
+                            Department.builder()
+                                    .dept_name("부서" + i)
+                                    .build()
+                    )
+                    .emp_phone_num("010-2472-2117")
+                    .emp_jumin("123456-1234567")
+                    .emp_internal_num("010-1234-5678")
+                    .emp_join_date(LocalDate.of(LocalDate.now().getYear(), (int)(Math.random() * 11 + 1), (int)(Math.random() * 25 + 1)))
+                    .build();
+            employeeList.add(employee);
+            employeeRepository.save(employee);
+            employeeRepository.save(
+                    Employee.builder()
+                            .emp_name("테스터" + i)
+                            .department(new Department())
+                            .build()
+            );
+            if(i % 4 == 0 && i != 0){
+                vacationRepository.save(
+                        Vacation.builder()
+                                .vac_start_date(LocalDate.now().minusDays(1))
+                                .vac_end_date(LocalDate.now().plusDays(1))
+                                .employee(employee)
+                                .build()
+                );
+            }
+
+            if(i % 5 == 0 && i != 0){
+                sickRepository.save(
+                        Sick.builder()
+                                .sick_start_date(LocalDate.now().minusDays(1))
+                                .sick_end_date(LocalDate.now().plusDays(1))
+                                .employee(employee)
+                                .build()
+                );
+            }
+        }
+
+        attendanceService.autoCreateAttendance();
+
+        attendanceService.onWork(employeeList.get(0));
+        attendanceService.onWork(employeeList.get(1));
+        attendanceService.onWork(employeeList.get(2));
+        attendanceService.offWork(employeeList.get(2));
 
     }
 }
---
+++ src/test/java/team/okky/personnel_management/service/WorkServiceTest.java
@@ -0,0 +1,109 @@
+package team.okky.personnel_management.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import team.okky.personnel_management.domain.Department;
+import team.okky.personnel_management.domain.Work;
+import team.okky.personnel_management.dto.WorkSearchDTO;
+import team.okky.personnel_management.repository.DepartmentRepository;
+import team.okky.personnel_management.repository.EmployeeRepository;
+import team.okky.personnel_management.dto.WorkFindDto;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Rollback(value = false)
+public class WorkServiceTest {
+    @Autowired WorkService workService;
+    @Autowired DepartmentRepository departmentRepository;
+    @Autowired EmployeeRepository employeeRepository;
+
+    @Test
+    public void 업무추가() throws Exception{
+        //given
+        Work work = new Work();
+        work.setWork_name("VR");
+        work.setWork_charge_name("jungha");
+        work.setWork_start_date(LocalDate.of(2021,1,3));
+        work.setWork_end_date(LocalDate.of(2022,6,16));
+        work.setDepartment(departmentRepository.findOne(2L));
+
+        //when, then
+        assertEquals(work,workService.save(work),"업무 추가되었다.");
+    }
+
+    @Test
+    public void 업무전체검색() throws Exception{
+        //given
+        List<WorkFindDto> findWorks = workService.findAll();
+        int findWorksSize = findWorks.size();
+        int countDTO=0;
+
+        //when
+        for (WorkFindDto dto : findWorks) {
+            countDTO++;
+            System.out.println("업무명=" + dto.getWork_name());
+            System.out.println("업무명과 연관된 부서명 =" + dto.getDept_name());
+            System.out.println("업무를 맡은 직원들=" + dto.getEmployees());
+            System.out.println("업무 시작일 =" + dto.getWork_start_date());
+            System.out.println("업무 진행상태="+dto.getWorkStatus());
+            System.out.println("====================================");
+        }
+
+        //then
+        assertEquals(findWorksSize,countDTO,"업무전체가 검색되었습니다.");
+    }
+
+    @Test
+    public void 업무상세검색() throws Exception{
+        //given
+        WorkSearchDTO workSearch = new WorkSearchDTO();
+        workSearch.setNameType("empName");
+        workSearch.setName("honggildong");
+
+        //when
+        List<WorkFindDto> findWorks = workService.filteringList(workSearch);
+        for (WorkFindDto dto : findWorks) {
+            System.out.println("업무명=" + dto.getWork_name());
+            System.out.println("업무명과 연관된 부서명 =" + dto.getDept_name());
+            System.out.println("업무를 맡은 직원들=" + dto.getEmployees());
+            System.out.println("업무 시작일 =" + dto.getWork_start_date());
+            System.out.println("업무 진행상태="+dto.getWorkStatus());
+            System.out.println("====================================");
+        }
+
+        List<String> empList = findWorks.get(0).getEmployees();
+
+        //then
+        assertTrue(empList.contains(workSearch.getName()));
+    }
+
+    @Test
+    public void 업무수정() throws Exception{
+        //given
+        Work work = workService.findOne(1L);
+        String chargeName = work.getWork_charge_name();
+
+        //when
+        Work updateWork = workService.update(work.getWork_id(),"인공지능",1L,"honggildong",
+                LocalDate.of(2020,1,9),
+                LocalDate.of(2020,12,21));
+
+        //then
+        if(chargeName.equals(updateWork.getWork_charge_name())){
+            Assertions.fail("업무 상세정보가 수정되지 않았습니다.");
+        }
+        else {
+            Assertions.assertTrue(true,"업무 상세정보가 수정되었습니다.");
+        }
+
+    }
+}

a1d2dfa1e1916e248318b141c012728f757e9e6c
Boeun05 <71836751+Boeun05@users.noreply.github.com>
2021-01-29 04:44:32 +0900

    set personnelStatus

---
+++ frontend/src/components/Table/PersonnelStatusRow.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function PersonnelStatusRow({ data }) {
+  return (
+    <tr>
+      <td>{data}</td>
+      <td>{data}</td>
+      <td>{data}</td>
+      <td>{data}</td>
+      <td>{data}</td>
+      <td>{data}</td>
+    </tr>
+  );
+}
+
+export default PersonnelStatusRow;

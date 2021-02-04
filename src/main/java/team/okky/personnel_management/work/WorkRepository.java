21a0b4f6bb5c85c1275fba5a322b80072919c5ec
songyen <yeeun4150@gmail.com>
2021-02-04 18:38:24 +0900

    update Work

--- src/main/java/team/okky/personnel_management/work/WorkRepository.java
+++ src/main/java/team/okky/personnel_management/work/WorkRepository.java
@@ -1,6 +1,7 @@
 package team.okky.personnel_management.work;
 
 import org.springframework.stereotype.Repository;
+import team.okky.personnel_management.utils.dto.PageRequestDTO;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -21,27 +22,70 @@ public class WorkRepository {
         return em.find(Work.class, id);
     }
 
-    public Work findByWorkName(String workName){
-        TypedQuery<Work> query = em.createQuery("select w from Work w where w.workName = :name",Work.class)
-                .setParameter("name",workName);
-        return query.getSingleResult();
+    public List<Work> findByWorkName(String workName, PageRequestDTO pageRequestDTO){
+        return em.createQuery("select w from Work w where w.workName = :name",Work.class)
+                .setParameter("name",workName)
+                .setFirstResult(pageRequestDTO.getPage())
+                .setMaxResults(pageRequestDTO.getSize())
+                .getResultList();
     }
 
-    public Work findByEmpName(String empName){
-        TypedQuery<Work> query = em.createQuery("select w from Work w join Employee e on w = e.work and e.empName = :name",Work.class)
-                .setParameter("name",empName);
-        return query.getSingleResult();
+    public int findTotalByWorkName(String workName){
+        return em.createQuery("select count(w) from Work w where w.workName = :name",Long.class)
+                .setParameter("name",workName)
+                .getSingleResult().intValue();
+    }
+
+    public List<Work> findByEmpName(String empName, PageRequestDTO pageRequestDTO){
+        return em.createQuery("select e.work from Evaluation e join e.employee " +
+                "on e.employee.empName =: name order by e.work.workEndDate desc",Work.class)
+                .setParameter("name",empName)
+                .setFirstResult(pageRequestDTO.getPage())
+                .setMaxResults(pageRequestDTO.getSize())
+                .getResultList();
+    }
+
+    public int findTotalByEmpName(String empName){
+        return em.createQuery("select count(e.work) from Evaluation e join e.employee on e.employee.empName =: name",Long.class)
+                .setParameter("name",empName)
+                .getSingleResult().intValue();
+    }
+
+    public List<Work> findByDeptName(String deptName, PageRequestDTO pageRequestDTO){
+        return em.createQuery("select w from Work w join fetch w.department where w.department.deptName = :name")
+                .setParameter("name",deptName)
+                .setFirstResult(pageRequestDTO.getPage())
+                .setMaxResults(pageRequestDTO.getSize())
+                .getResultList();
     }
 
     public List<Work> findByDeptName(String deptName){
-        return em.createQuery("select w from Work w join Department d on w.department = d and d.deptName = :name")
+        return em.createQuery("select w from Work w join fetch w.department where w.department.deptName = :name")
                 .setParameter("name",deptName)
+                .getResultList();
+    }
+
+    public int findTotalByDeptName(String deptName){
+        return em.createQuery("select count(w) from Work w join fetch w.department where w.department.deptName = :name",Long.class)
+                .setParameter("name",deptName)
+                .getSingleResult().intValue();
+    }
+
+    public List<Work> findAll(PageRequestDTO pageRequestDTO) {
+        return em.createQuery("select w from Work w order by w.workEndDate desc")
+                .setFirstResult(pageRequestDTO.getPage())
+                .setMaxResults(pageRequestDTO.getSize())
                 .getResultList();
     }
 
     public List<Work> findAll() {
         return em.createQuery("select w from Work w order by w.workEndDate desc")
                 .getResultList();
+    }
+
+    public int findTotal(){
+        return em.createQuery("select count(w) from Work w",Long.class)
+                .getSingleResult().intValue();
     }
 
     public List<Long> findWorkId(){
